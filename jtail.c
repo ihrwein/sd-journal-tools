6edce5241619c570c55e908cdba38266108d2ba2
Tibor Benke <btibi@sch.bme.hu>
2014-07-02 11:31:58 +0200

    Add the POLL directive.
    Add a #define POLL directive to be able to choose the wait mechanism between
    poll() or sd_journal_wait().

--- jtail.c
+++ jtail.c
@@ -1,4 +1,5 @@
 #include <systemd/sd-journal.h>
+#include <sys/poll.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -6,6 +7,7 @@
 
 #define INFINITE_WAIT -1
 #define LAST_MESSAGE_NUMBER 10
+#define POLL 0
 
 void
 seek_back_by_n(sd_journal* journal, int n)
@@ -17,6 +19,46 @@ seek_back_by_n(sd_journal* journal, int n)
 	{
 		ret = sd_journal_previous(journal);
 		print_err("Failed to seek back in the journal: %s\n", ret);
+	}
+}
+
+int
+journal_wait(sd_journal* journal)
+{
+#if POLL
+	struct pollfd pollfd;
+
+    pollfd.fd = sd_journal_get_fd(journal);
+    pollfd.events = sd_journal_get_events(journal);
+    poll(&pollfd, 1, INFINITE_WAIT);
+    return sd_journal_process(journal);
+#else
+	return sd_journal_wait(journal, INFINITE_WAIT);
+#endif
+}
+
+void
+process_available_messages(sd_journal* journal)
+{
+	int ret;
+
+	for (;;) 
+	{
+		ret = sd_journal_next(journal);
+		
+		/* no more message, go to sleep */
+		if (ret == 0)
+		{
+			break;
+		}
+		else if (ret < 0)			
+		{
+			fprintf(stderr, "Failed to read the next message: %s\n", strerror(-ret));
+    		return;
+		}
+		
+		print_fields(journal);
+		printf("--------------------------------------------------------\n");
 	}
 }
 
@@ -36,26 +78,11 @@ main()
 
 	seek_back_by_n(journal, LAST_MESSAGE_NUMBER);	
 
-	while ((ret = sd_journal_wait(journal, INFINITE_WAIT)) >= 0)
+	process_available_messages(journal);
+
+	while ((ret = journal_wait(journal)) >= 0)
 	{
-		for (;;) 
-		{
-			ret = sd_journal_next(journal);
-			
-			/* no more message, go to sleep */
-			if (ret == 0)
-			{
-				break;
-			}
-			else if (ret < 0)			
-			{
-				fprintf(stderr, "Failed to seek to the tail of the journal: %s\n", strerror(-ret));
-        		return 1;
-			}
-			
-			print_fields(journal);
-			printf("--------------------------------------------------------\n");
-		}	
+		process_available_messages(journal);		
 	}
 
 	sd_journal_close(journal);
