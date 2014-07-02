f4624fd3cd3236f5ef4f8904d936f73f288fa7c0
Tibor Benke <btibi@sch.bme.hu>
2014-07-02 17:36:37 +0200

    Add ivykis support for jtail and remove the previously used ones.

--- jtail.c
+++ jtail.c
@@ -1,41 +1,19 @@
 #include <systemd/sd-journal.h>
-#include <sys/poll.h>
 #include <stdio.h>
 #include <string.h>
+#include <iv.h>
 
 #include "utils.h"
 
-#define INFINITE_WAIT -1
 #define LAST_MESSAGE_NUMBER 10
-#define POLL 0
 
-void
-seek_back_by_n(sd_journal* journal, int n)
+struct iv_fd journal_fd;
+
+typedef struct _JournalPrivateData
 {
-	int i;
-	int ret;
+	sd_journal* journal;
+} JournalPrivateData;
 
-	for (i = 0; i < LAST_MESSAGE_NUMBER; i++)
-	{
-		ret = sd_journal_previous(journal);
-		print_err("Failed to seek back in the journal: %s\n", ret);
-	}
-}
-
-int
-journal_wait(sd_journal* journal)
-{
-#if POLL
-	struct pollfd pollfd;
-
-    pollfd.fd = sd_journal_get_fd(journal);
-    pollfd.events = sd_journal_get_events(journal);
-    poll(&pollfd, 1, INFINITE_WAIT);
-    return sd_journal_process(journal);
-#else
-	return sd_journal_wait(journal, INFINITE_WAIT);
-#endif
-}
 
 void
 process_available_messages(sd_journal* journal)
@@ -62,11 +40,41 @@ process_available_messages(sd_journal* journal)
 	}
 }
 
+
+void
+process_available_messages_callback(void *data)
+{
+	JournalPrivateData* priv_data = (JournalPrivateData*) data;
+	sd_journal* journal = priv_data->journal;
+
+	sd_journal_process(journal);
+	process_available_messages(journal); 
+}
+
+
+void
+seek_back_by_n(sd_journal* journal, int n)
+{
+	int i;
+	int ret;
+
+	for (i = 0; i < LAST_MESSAGE_NUMBER; i++)
+	{
+		ret = sd_journal_previous(journal);
+		print_err("Failed to seek back in the journal: %s\n", ret);
+	}
+}
+
+
 int
 main()
 {
 	int ret;
 	sd_journal *journal;
+	JournalPrivateData priv_data;
+	
+	iv_init();
+	IV_FD_INIT(&journal_fd);
 
 	ret = sd_journal_open(&journal, SD_JOURNAL_LOCAL_ONLY);
 	
@@ -77,14 +85,19 @@ main()
 		return 1;
 
 	seek_back_by_n(journal, LAST_MESSAGE_NUMBER);	
-
+	/* process the last LAST_MESSAGE_NUMBER messages */
 	process_available_messages(journal);
+	
+	priv_data.journal = journal;
 
-	while ((ret = journal_wait(journal)) >= 0)
-	{
-		process_available_messages(journal);		
-	}
+	journal_fd.fd = sd_journal_get_fd(journal);
+	journal_fd.handler_in = process_available_messages_callback;
+	journal_fd.cookie = &priv_data;
+	iv_fd_register(&journal_fd);
 
+	iv_main();
+
+	iv_deinit();
 	sd_journal_close(journal);
 	return 0;
 }
