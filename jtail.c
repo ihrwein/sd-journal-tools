e06b54a47ca3ac9315e4735c5586e553f0b66682
Tibor Benke <ihrwein@gmail.com>
2014-07-03 17:03:33 +0200

    Reformat code as GNU style.

--- jtail.c
+++ jtail.c
@@ -11,93 +11,93 @@ struct iv_fd journal_fd;
 
 typedef struct _JournalPrivateData
 {
-	sd_journal* journal;
+  sd_journal* journal;
 } JournalPrivateData;
 
 
 void
 process_available_messages(sd_journal* journal)
 {
-	int ret;
+  int ret;
 
-	for (;;) 
-	{
-		ret = sd_journal_next(journal);
-		
-		/* no more message, go to sleep */
-		if (ret == 0)
-		{
-			break;
-		}
-		else if (ret < 0)			
-		{
-			fprintf(stderr, "Failed to read the next message: %s\n", strerror(-ret));
-    		return;
-		}
-		
-		print_fields(journal);
-		printf("--------------------------------------------------------\n");
-	}
+  for (;;)
+    {
+      ret = sd_journal_next(journal);
+
+      /* no more message, go to sleep */
+      if (ret == 0)
+        {
+          break;
+        }
+      else if (ret < 0)
+        {
+          fprintf(stderr, "Failed to read the next message: %s\n", strerror(-ret));
+          return;
+        }
+
+      print_fields(journal);
+      printf("--------------------------------------------------------\n");
+    }
 }
 
 
 void
 process_available_messages_callback(void *data)
 {
-	JournalPrivateData* priv_data = (JournalPrivateData*) data;
-	sd_journal* journal = priv_data->journal;
+  JournalPrivateData* priv_data = (JournalPrivateData*) data;
+  sd_journal* journal = priv_data->journal;
 
-	sd_journal_process(journal);
-	process_available_messages(journal); 
+  sd_journal_process(journal);
+  process_available_messages(journal);
 }
 
 
 void
 seek_back_by_n(sd_journal* journal, int n)
 {
-	int i;
-	int ret;
+  int i;
+  int ret;
 
-	for (i = 0; i < LAST_MESSAGE_NUMBER; i++)
-	{
-		ret = sd_journal_previous(journal);
-		print_err("Failed to seek back in the journal: %s\n", ret);
-	}
+  for (i = 0; i < LAST_MESSAGE_NUMBER; i++)
+    {
+      ret = sd_journal_previous(journal);
+      print_err("Failed to seek back in the journal: %s\n", ret);
+    }
 }
 
 
 int
 main()
 {
-	int ret;
-	sd_journal *journal;
-	JournalPrivateData priv_data;
-	
-	iv_init();
-	IV_FD_INIT(&journal_fd);
+  int ret;
+  sd_journal *journal;
+  JournalPrivateData priv_data;
 
-	ret = sd_journal_open(&journal, SD_JOURNAL_LOCAL_ONLY);
-	
-	ret = sd_journal_seek_tail(journal);
-	ret = print_err("Failed to seek to the tail of the journal: %s\n", ret);
-	
-	if (ret)
-		return 1;
+  iv_init();
+  IV_FD_INIT(&journal_fd);
 
-	seek_back_by_n(journal, LAST_MESSAGE_NUMBER);	
-	/* process the last LAST_MESSAGE_NUMBER messages */
-	process_available_messages(journal);
-	
-	priv_data.journal = journal;
+  ret = sd_journal_open(&journal, SD_JOURNAL_LOCAL_ONLY);
 
-	journal_fd.fd = sd_journal_get_fd(journal);
-	journal_fd.handler_in = process_available_messages_callback;
-	journal_fd.cookie = &priv_data;
-	iv_fd_register(&journal_fd);
+  ret = sd_journal_seek_tail(journal);
+  ret = print_err("Failed to seek to the tail of the journal: %s\n", ret);
 
-	iv_main();
+  if (ret)
+    return 1;
 
-	iv_deinit();
-	sd_journal_close(journal);
-	return 0;
+  seek_back_by_n(journal, LAST_MESSAGE_NUMBER);
+  /* process the last LAST_MESSAGE_NUMBER messages */
+  process_available_messages(journal);
+
+  priv_data.journal = journal;
+
+  journal_fd.fd = sd_journal_get_fd(journal);
+  journal_fd.handler_in = process_available_messages_callback;
+  journal_fd.cookie = &priv_data;
+  iv_fd_register(&journal_fd);
+
+  iv_main();
+
+  iv_deinit();
+  sd_journal_close(journal);
+  return 0;
 }
--- utils.h
+++ utils.h
@@ -11,73 +11,73 @@
 void
 split_data(const char* data, size_t data_length, char* key, char* value)
 {
-    int i, j;
+  int i, j;
 
-    for (i = 0; data[i] != '=' && i < data_length; i++)
-        key[i] = data[i];
+  for (i = 0; data[i] != '=' && i < data_length; i++)
+    key[i] = data[i];
 
-    key[i++] = '\0';
+  key[i++] = '\0';
 
-    for (j = i; j < data_length; j++)
-        value[j - i] = data[j];
+  for (j = i; j < data_length; j++)
+    value[j - i] = data[j];
 
-    value[j - i] = '\0';
+  value[j - i] = '\0';
 }
 
 void
 lookup_catalog(sd_journal* journal)
 {
-    char *data;
-    int ret;
+  char *data;
+  int ret;
 
-    ret = sd_journal_get_catalog(journal, &data);
+  ret = sd_journal_get_catalog(journal, &data);
 
-    if (ret == 0)
+  if (ret == 0)
     {
-        printf("%s\n", data);
-        free(data);
+      printf("%s\n", data);
+      free(data);
     }
-    else if (ret == (-1* ENOENT))
+  else if (ret == (-1* ENOENT))
     {
-    // no catalog entry
+      // no catalog entry
     }
 }
 
 void
 print_fields(sd_journal *j)
 {
-    const void *data;
-    size_t length;
-    
-    char key[KEY_LENGTH];
-    char value[VALUE_LENGTH];
+  const void *data;
+  size_t length;
 
-    lookup_catalog(j);
+  char key[KEY_LENGTH];
+  char value[VALUE_LENGTH];
 
-    SD_JOURNAL_FOREACH_DATA(j, data, length)
-    {
-        if (length < KEY_LENGTH + VALUE_LENGTH)
-        {
-            split_data(data, length, key, value);
-            printf("%s %s\n", key, value);
-        }
-        else 
-        {
-            printf("%.*s\n", (int) length, (char*) data);
-        }
-    }
+  lookup_catalog(j);
+
+  SD_JOURNAL_FOREACH_DATA(j, data, length)
+  {
+    if (length < KEY_LENGTH + VALUE_LENGTH)
+      {
+        split_data(data, length, key, value);
+        printf("%s %s\n", key, value);
+      }
+    else
+      {
+        printf("%.*s\n", (int) length, (char*) data);
+      }
+  }
 }
 
 
 int
 print_err(char* errmsg, int errcode)
 {
-    if (errcode < 0)
+  if (errcode < 0)
     {
-        fprintf(stderr, errmsg, strerror(-errcode));
-        return 1;
+      fprintf(stderr, errmsg, strerror(-errcode));
+      return 1;
     }
-    return 0;
+  return 0;
 }
 
 #endif
