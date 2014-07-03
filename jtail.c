6b40a979a9bcf7378fd88c5a42279a610c2a81ad
Tibor Benke <ihrwein@gmail.com>
2014-07-03 17:02:37 +0200

    Reformat code as GNU style.

--- jtail.c
+++ jtail.c
@@ -12,79 +12,79 @@
 void
 seek_back_by_n(sd_journal* journal, int n)
 {
-	int i;
-	int ret;
+  int i;
+  int ret;
 
-	for (i = 0; i < LAST_MESSAGE_NUMBER; i++)
-	{
-		ret = sd_journal_previous(journal);
-		print_err("Failed to seek back in the journal: %s\n", ret);
-	}
+  for (i = 0; i < LAST_MESSAGE_NUMBER; i++)
+    {
+      ret = sd_journal_previous(journal);
+      print_err("Failed to seek back in the journal: %s\n", ret);
+    }
 }
 
 int
 journal_wait(sd_journal* journal)
 {
 #if POLL
-	struct pollfd pollfd;
+  struct pollfd pollfd;
 
-    pollfd.fd = sd_journal_get_fd(journal);
-    pollfd.events = sd_journal_get_events(journal);
-    poll(&pollfd, 1, INFINITE_WAIT);
-    return sd_journal_process(journal);
+  pollfd.fd = sd_journal_get_fd(journal);
+  pollfd.events = sd_journal_get_events(journal);
+  poll(&pollfd, 1, INFINITE_WAIT);
+  return sd_journal_process(journal);
 #else
-	return sd_journal_wait(journal, INFINITE_WAIT);
+  return sd_journal_wait(journal, INFINITE_WAIT);
 #endif
 }
 
 void
 process_available_messages(sd_journal* journal)
 {
-	int ret;
+  int ret;
 
-	for (;;) 
-	{
-		ret = sd_journal_next(journal);
-		
-		/* no more message, go to sleep */
-		if (ret == 0)
-		{
-			break;
-		}
-		else if (ret < 0)			
-		{
-			fprintf(stderr, "Failed to read the next message: %s\n", strerror(-ret));
-    		return;
-		}
-		
-		print_fields(journal);
-		printf("--------------------------------------------------------\n");
-	}
+  for (;;)
+    {
+      ret = sd_journal_next(journal);
+
+      /* no more message, go to sleep */
+      if (ret == 0)
+        {
+          break;
+        }
+      else if (ret < 0)
+        {
+          fprintf(stderr, "Failed to read the next message: %s\n", strerror(-ret));
+          return;
+        }
+
+      print_fields(journal);
+      printf("--------------------------------------------------------\n");
+    }
 }
 
 int
 main()
 {
-	int ret;
-	sd_journal *journal;
+  int ret;
+  sd_journal *journal;
 
-	ret = sd_journal_open(&journal, SD_JOURNAL_LOCAL_ONLY);
-	
-	ret = sd_journal_seek_tail(journal);
-	ret = print_err("Failed to seek to the tail of the journal: %s\n", ret);
-	
-	if (ret)
-		return 1;
+  ret = sd_journal_open(&journal, SD_JOURNAL_LOCAL_ONLY);
 
-	seek_back_by_n(journal, LAST_MESSAGE_NUMBER);	
+  ret = sd_journal_seek_tail(journal);
+  ret = print_err("Failed to seek to the tail of the journal: %s\n", ret);
 
-	process_available_messages(journal);
+  if (ret)
+    return 1;
 
-	while ((ret = journal_wait(journal)) >= 0)
-	{
-		process_available_messages(journal);		
-	}
+  seek_back_by_n(journal, LAST_MESSAGE_NUMBER);
 
-	sd_journal_close(journal);
-	return 0;
+  process_available_messages(journal);
+
+  while ((ret = journal_wait(journal)) >= 0)
+    {
+      process_available_messages(journal);
+    }
+
+  sd_journal_close(journal);
+  return 0;
 }
--- utils.h
+++ utils.h
@@ -4,23 +4,23 @@
 void
 print_fields(sd_journal *j)
 {
-    const void *data;
-    size_t length;
-    SD_JOURNAL_FOREACH_DATA(j, data, length)
-    {
-        printf("%.*s\n", (int) length, (char*) data);
-    }
+  const void *data;
+  size_t length;
+  SD_JOURNAL_FOREACH_DATA(j, data, length)
+  {
+    printf("%.*s\n", (int) length, (char*) data);
+  }
 }
 
 int
 print_err(char* errmsg, int errcode)
 {
-	if (errcode < 0)
-	{
-        fprintf(stderr, errmsg, strerror(-errcode));
-        return 1;
+  if (errcode < 0)
+    {
+      fprintf(stderr, errmsg, strerror(-errcode));
+      return 1;
     }
-	return 0;
+  return 0;
 }
 
 #endif
